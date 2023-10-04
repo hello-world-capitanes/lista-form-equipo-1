04ab188dff46f8a858f8b36736f2a117ebb28cd6
David Hermosilla Duro <david.hermosilla@babelgroup.com>
2023-10-04 12:51:35 +0200

    List + Modal working

--- list-form/src/App.jsx
+++ list-form/src/App.jsx
@@ -1,36 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Lista from './Lista/Lista'
-import Modal from './components/Modal/Modal';
+import { useState } from 'react';
+import './App.css';
+import Lista from './Lista/Lista';
 
 function App() {
   const [list, setList] = useState([
-    {texto: "Primero",
-    logico: "Si",
-    seleccion:"A"},
-    {texto: "Segund",
-    logico: "Siqdwqwd",
-    seleccion:"B"},
-    {texto: "Tercero",
-    logico: "Siqwdqd",
-    seleccion:"C"},
-    {texto: "Cuarto",
-    logico: "Siyht",
-    seleccion:"D"},
-  ])
-  
-  const info = {
-    texto: 'Texto',
-    logico: 'Logico',
-    seleccion: 'Seleccion',
-  };
-  const [isModalActive, setIsModalActive] = useState(false);
+    { texto: 'Primero', logico: 'Si', seleccion: 'A' },
+    { texto: 'Segund', logico: 'Siqdwqwd', seleccion: 'B' },
+    { texto: 'Tercero', logico: 'Siqwdqd', seleccion: 'C' },
+    { texto: 'Cuarto', logico: 'Siyht', seleccion: 'D' },
+  ]);
+
   return (
     <>
-      <Lista elementos={list} setList={setList}/>
-      <Modal info={info} isModalActive={isModalActive} setIsModalActive={setIsModalActive} />
+      <Lista elementos={list} setList={setList} />
     </>
   );
 }
--- list-form/src/Lista/Lista.jsx
+++ list-form/src/Lista/Lista.jsx
@@ -1,46 +1,57 @@
-import { useState } from 'react'
-import Button from '../Button/Button'
-import './index.css'
+import { useState } from 'react';
+import Button from '../Button/Button';
+import Modal from '../components/Modal/Modal';
+import './index.css';
 
-function Lista({elementos, setList}) {
-    const [isModalActive, setIsModalActive] = useState(false)
+function Lista({ elementos, setList }) {
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [modalItem, setModalItem] = useState({});
 
-    function handleRemove(index) {
-        setList(prevList => {
-            const copy = [...prevList]
-            copy.splice(index, 1)
-            return copy
-        })
-    }
+  function handleRemove(index) {
+    setList((prevList) => {
+      const copy = [...prevList];
+      copy.splice(index, 1);
+      return copy;
+    });
+  }
 
-    function handleUpDown(index, upDown) {
-        const suma = upDown  ? -1 : 1
-        setList(prevList => {
-            const copy= [...prevList]
-            const aux = copy[index+suma]
-            copy[index+suma] = copy[index]
-            copy[index] = aux
-            return copy
-        })
-    }
-    function handleSelect() {
-        setIsModalActive(true)
-    }
-    return (
-        <ul className='lista-div'>
-            {   
-                elementos.map((item, index) => 
-                    <li className="list-ele" key={`${item.texto}${item.logico}`} onDoubleClick={() => handleSelect()}>
-                        {item.texto}
-                        <Button className="button" onClick={() => handleRemove(index)} text="Delete"/>
-                        {!(index==elementos.length-1)  && <Button className="button" onClick={() => handleUpDown(index, true)} text="Up"/>}
-                        {!(index==0) && <Button className="button" onClick={() =>handleUpDown(index, false)} text="Down"/>}
-                        <Modal isModalActive={isModalActive} setIsModalActive={setIsModalActive}/>
-                    </li>
-                )
-            }
-        </ul>
-    )
+  function handleUpDown(index, upDown) {
+    const suma = upDown ? -1 : 1;
+    setList((prevList) => {
+      const copy = [...prevList];
+      const aux = copy[index + suma];
+      copy[index + suma] = copy[index];
+      copy[index] = aux;
+      return copy;
+    });
+  }
+  function handleSelect(item) {
+    setModalItem(item);
+    setIsModalActive(true);
+  }
+  return (
+    <>
+      <ul className="lista-div">
+        {elementos.map((item, index) => (
+          <li
+            className="list-ele"
+            key={`${item.texto}${item.logico}`}
+            onDoubleClick={() => handleSelect(item)}
+          >
+            {item.texto}
+            <Button className="button" onClick={() => handleRemove(index)} text="Delete" />
+            {!(index == elementos.length - 1) && (
+              <Button className="button" onClick={() => handleUpDown(index, true)} text="Up" />
+            )}
+            {!(index == 0) && (
+              <Button className="button" onClick={() => handleUpDown(index, false)} text="Down" />
+            )}
+          </li>
+        ))}
+      </ul>
+      <Modal info={modalItem} isModalActive={isModalActive} setIsModalActive={setIsModalActive} />
+    </>
+  );
 }
 
-export default Lista
+export default Lista;
