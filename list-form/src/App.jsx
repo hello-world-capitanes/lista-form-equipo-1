c1bc027bb0c34c541ae6fef8fd725962d5ea73a7
David Hermosilla Duro <david.hermosilla@babelgroup.com>
2023-10-05 08:40:51 +0200

    Small changes

--- list-form/src/App.jsx
+++ list-form/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-import Formulario from './components/Formulario/Formulario'
+import { useState } from 'react';
+import Formulario from './components/Formulario/Formulario';
 import Lista from './Lista/Lista';
+import './App.css';
 
 function App() {
   const [list, setList] = useState([]);
